049340ba38355dd20b4338f7c20440609660d63c
Basheer Shahrour <shahrourbasheer1997@gmail.com>
2020-08-02 20:50:14 +0300

    orgnizing the files

--- wifiWebSoc/wifiWebSoc.ino
+++ wifiWebSoc/wifiWebSoc.ino
@@ -7,15 +7,14 @@ const char *password = "welcometolab";
 int temp;
 int pTemp = 0;
 int fanSpeed = 150;
-char* currDir = "";
-int INA = 4;  //D2
-int INB = 5;  //D1
+char *currDir = "";
+int INA = 4;      //D2
+int INB = 5;      //D1
 int lampPin = 2;  //D4
-int servoPin = 0;  //D3
+int servoPin = 0; //D3
 int angle = 90;
 SocketIoClient webSocket;
 Servo servo;
-
 
 void connectHandler(const char *payload, size_t length)
 {
@@ -26,39 +25,61 @@ void connectHandler(const char *payload, size_t length)
 void messageHandler(const char *payload, size_t length)
 {
   Serial.printf("got message: %s\n", payload);
-   if(strcmp(payload, "lampOn") == 0) {
+  if (strcmp(payload, "lampOn") == 0)
+  {
     digitalWrite(lampPin, HIGH);
-  } else if(strcmp(payload, "lampOff") == 0) {
+  }
+  else if (strcmp(payload, "lampOff") == 0)
+  {
     digitalWrite(lampPin, LOW);
-  } else if(strcmp(payload, "fanLeft") == 0) {
-      currDir = "left";
-      digitalWrite(INB,LOW);
-      analogWrite(INA, fanSpeed);
-  } else if(strcmp(payload, "fanRight") == 0) {
-      currDir = "right";
-      digitalWrite(INA,LOW);
-      analogWrite(INB, fanSpeed);
-  } else if(strcmp(payload, "fanSpeed_1") == 0) {
+  }
+  else if (strcmp(payload, "fanLeft") == 0)
+  {
+    currDir = "left";
+    digitalWrite(INB, LOW);
+    analogWrite(INA, fanSpeed);
+  }
+  else if (strcmp(payload, "fanRight") == 0)
+  {
+    currDir = "right";
+    digitalWrite(INA, LOW);
+    analogWrite(INB, fanSpeed);
+  }
+  else if (strcmp(payload, "fanSpeed_1") == 0)
+  {
     fanSpeed = 50;
     setSpeed();
-  } else if(strcmp(payload, "fanSpeed_2") == 0) {
+  }
+  else if (strcmp(payload, "fanSpeed_2") == 0)
+  {
     fanSpeed = 150;
     setSpeed();
-  } else if(strcmp(payload, "fanSpeed_3") == 0) {
+  }
+  else if (strcmp(payload, "fanSpeed_3") == 0)
+  {
     fanSpeed = 255;
     setSpeed();
-  } else if(strcmp(payload, "doorLeft") == 0) {
+  }
+  else if (strcmp(payload, "doorLeft") == 0)
+  {
     angle += 70;
-    if(angle < 170) servo.write(angle); 
-  } else if(strcmp(payload, "doorRight") == 0) {
+    if (angle < 170)
+      servo.write(angle);
+  }
+  else if (strcmp(payload, "doorRight") == 0)
+  {
     angle -= 70;
-    if(angle > 10) servo.write(angle); 
+    if (angle > 10)
+      servo.write(angle);
   }
 }
 
-void setSpeed() {
-  if(strcmp(currDir, "left") == 0) analogWrite(INA, fanSpeed);
-    else analogWrite(INB, fanSpeed);
+void setSpeed()
+{
+  if (strcmp(currDir, "left") == 0)
+    analogWrite(INA, fanSpeed);
+  else
+    analogWrite(INB, fanSpeed);
 }
 
 void setup()
@@ -94,7 +115,6 @@ void setup()
   Serial.println(WiFi.localIP());
 
   webSocket.begin("192.168.137.79", 3030);
-//    webSocket.begin("arduino-ar-project.glitch.me", 80);
 
   webSocket.on("message", messageHandler);
   webSocket.on("connect", connectHandler);
@@ -111,15 +131,16 @@ void loop()
   webSocket.loop();
   temp = analogRead(A0);
   temp = temp * 0.48828125;
-  if(temp < pTemp) {
+  if (temp < pTemp)
+  {
     webSocket.emit("message", "\"dec\"");
-  } else if(temp > pTemp) {
+  }
+  else if (temp > pTemp)
+  {
     webSocket.emit("message", "\"inc\"");
   }
   pTemp = temp;
   Serial.println(temp);
-  
-  delay(500);
-  
 
+  delay(500);
 }
